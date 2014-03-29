f32c376da7697c7141c02738842e401d4e130175
DeProdigy <alex.hint.ah@gmail.com>
2014-03-29 18:51:04 -0400

    #5 defining a method on the scope

--- scripts.js
+++ scripts.js
@@ -9,4 +9,8 @@ function FirstCtrl($scope, Data) {
 
 function SecondCtrl($scope, Data) {
   $scope.data = Data;
+
+  $scope.reversedMessage = function(message) {
+    return message.split('').reverse().join('');
+  };
 }
