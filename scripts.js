1c0609d14e63e2e396ac53c0c5f2ad14a40ad9ec
DeProdigy <alex.hint.ah@gmail.com>
2014-03-29 19:05:08 -0400

    #6 filters

--- scripts.js
+++ scripts.js
@@ -3,14 +3,16 @@ myApp.factory('Data', function() {
   return {message: "I'm data from a service"};
 });
 
+myApp.filter('reverse', function(){
+  return function(text){
+    return text.split('').reverse().join('');
+  };
+});
+
 function FirstCtrl($scope, Data) {
   $scope.data = Data;
 }
 
 function SecondCtrl($scope, Data) {
   $scope.data = Data;
-
-  $scope.reversedMessage = function(message) {
-    return message.split('').reverse().join('');
-  };
 }
